2d9bc158e77ba2ee6736ba80e75e7df783be0348
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-31 00:08:09 +0530

    Added: Finding Frequency of words in Paragraph

--- src/com/bridgelabz/datastructure/HashTable.java
+++ src/com/bridgelabz/datastructure/HashTable.java
@@ -5,25 +5,33 @@ import java.util.Set;
 
 public class HashTable {
 
-	static void InputSentence(String userString) {
+	static void userInputSentence(String userString) {
+		/*
+		 * Declaring a HashMap Splitting the word of string and storing them in the
+		 * array.
+		 */
 		HashMap<String, Integer> frequency = new HashMap<String, Integer>();
-		String[] words = userString.split(" ");
+		String[] words = userString.split(" "); // splitting to find word
 		for (String word : words) {
 			if (frequency.containsKey(word)) {
 				frequency.put(word, frequency.get(word) + 1);
 			} else {
-				frequency.put(word, 1);
+				frequency.put(word, 1); // storing the word as key
 			}
 		}
 		Set<String> stringFrequency = frequency.keySet();
-		System.out.println("Frequency Of Words In Sentence: \n 'To Be Or Not To Be'");
+		System.out.println("Frequency Of Words In Sentence: \n'Paranoids are not paranoid because\n"
+				+ "they are paranoid but because they keep putting themselves deliberately into\n"
+				+ "paranoid avoidable situations' is : ");
 		for (String word : stringFrequency) {
-			System.out.println(word + " = " + frequency.get(word) + " times.");
+			if (frequency.get(word) > 1)
+				System.out.println(word + " = " + frequency.get(word) + " times.");
 
 		}
 	}
 
 	public static void main(String[] args) {
-		InputSentence("To Be Or Not To Be");
+		userInputSentence("Paranoids are not paranoid because they are paranoid but because "
+				+ "they keep putting themselves deliberately into paranoid avoidable situations");
 	}
 }
